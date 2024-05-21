3e0121c700a8742502ade88bd7eb5e7a22820ac6
Franco <francofumiere321@gmail.com>
2024-05-21 18:17:08 -0300

    integrada libreria Lodash

--- js/fetch.js
+++ js/fetch.js
@@ -2,5 +2,5 @@ let cardsArray;
 
 fetch('../assets/cards.json')
 .then( response => response.json() )
-.then( data => cardsArray = data )
+.then( data => cardsArray = _.shuffle(data) )
 .catch( err => console.error( 'error al cargar las cartas' , err ));
